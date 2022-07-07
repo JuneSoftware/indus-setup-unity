28eb12ef73b5422f416dc4330e59f7ec0bb8f90a
Valiant John <84314269+ValiantJohnCh@users.noreply.github.com>
2022-07-07 15:28:35 +0530

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -69,7 +69,7 @@ async function installUnityHub() {
         unityHubPath = 'C:/Program Files/Unity Hub/Unity Hub.exe';
         if (!fs.existsSync(unityHubPath)) {
             const installerPath = await tc.downloadTool('https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.exe');
-            await execute(`"${installerPath}" /s`);
+            await execute(`choco install unity-hub -y`);
             await execute(`del "${installerPath}"`);
         }
 
